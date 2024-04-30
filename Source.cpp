0674fe27103c77b8dd68791d9761f6bbfa203c60
Zer-01 <oko48ru@gmail.com>
2024-04-30 15:15:26 +0300

    comment4

--- Source.cpp
+++ Source.cpp
@@ -7,14 +7,14 @@ using std::cout;
 
 int main() {
 	SetConsoleOutputCP(1251);
-	std::ifstream in("exmpl1.txt"); //1
-	//std::ifstream in("exmpl2.txt"); //2
+	//std::ifstream in("exmpl1.txt"); //1
+	std::ifstream in("exmpl2.txt"); //2
 	std::ofstream out("out.txt");
 	if (!in || !out) {
 		std::cerr << "Ошибка работы с файлами\n";
 	} else {
 		try {
-			processFile(in, cout);
+			cout<< "\nIer: " << processFile(in, cout) << '\n';
 		}
 		catch (std::string& s) {
 			cout << "Ошибка: " << s << '\n';
--- intgrl_func.cpp
+++ intgrl_func.cpp
@@ -4,8 +4,8 @@
 
 double f(double x, double y)
 {
-    return y - x * x * (x - 3.0); //1
-    //return 1; //2
+    //return y - x * x * (x - 3.0); //1
+    return 2 * x; //2
 }
 
 void readFile(std::istream& stream, InData& data)
@@ -41,6 +41,7 @@ int processFile(std::istream& inStream, std::ostream& outStream)
 {
     InData data;
     readFile(inStream, data);
+    //std::cout << " A:" << data.A << " B:" << data.B << " C:" << data.C << " yc:" << data.yc << " hmin:" << data.hmin << " eps:" << data.eps << '\n';
     double h = (data.B - data.A) / 10.0;
     double x, endPoint, y = data.yc;
     int stepsCount = 0, errorStepsCount = 0, icod = 0;
@@ -58,43 +59,59 @@ int processFile(std::istream& inStream, std::ostream& outStream)
 
     double localError = 0;
     while (data.direction == LR && x<endPoint || data.direction == RL && x>endPoint) {
+        //std::cout << "H: " << h << '\n';
         double y2 = rungeK2(x, y, h);
         double y3 = rungeK3(x, y, h);
         localError = abs(y3 - y2);
 
         if (localError < data.eps) {
             icod = 0;
-            if (stepsCount != 0)
-                y = y2;
-            printStep(outStream, x, y, localError);
-            stepsCount++;
+            
+            //if (stepsCount != 0) {
+            //    //y = y2;
+            //    printStep(outStream, x, y, localError);
+            //}
+            /*printStep(outStream, x, y, localError);
+            y = y2;
+            stepsCount++;*/
 
-            if (localError < data.eps / 4) {
+            /*if (localError < data.eps / 4) {
                 h *= 2.0;
-            }
+            }*/
 
-            if (data.direction == LR && endPoint - (x + h) < data.hmin || data.direction == RL && x - (endPoint + h) < data.hmin) {
+            if (data.direction == LR && endPoint - (x + h) < data.hmin || data.direction == RL && x + h - endPoint < data.hmin) {
+                //std::cout << "H: " << h << '\n';
+                printStep(outStream, x, y, localError);
                 if (data.direction == LR && endPoint - x >= 2.0 * data.hmin || data.direction == RL && x - endPoint >= 2.0 * data.hmin) {
                     double oldx = x;
                     x = (data.direction == LR) ? endPoint - data.hmin : endPoint + data.hmin;
-                    y = rungeK2(x, y, x - oldx);
-                    printStep(outStream, x, y, localError);
+                    double ynew = rungeK2(oldx, y, x - oldx);
+                    printStep(outStream, x, ynew, localError);
+                    y = ynew;
                 } else if (data.direction == LR && endPoint - x <= 1.5 * data.hmin || data.direction == RL && x - endPoint <= 1.5 * data.hmin) {
-                    x = endPoint;
+                    x = x;
                 }
                 else if ((data.direction == LR && endPoint - x > 1.5 * data.hmin && endPoint - x < 2.0 * data.hmin) 
                     || (data.direction == RL && x - endPoint > 1.5 * data.hmin && x - endPoint < 2.0 * data.hmin)) {
+                    double oldx = x;
                     x = (data.direction == LR) ? x + (endPoint - x) / 2.0 : x - (x - endPoint) / 2.0;
-                    y = rungeK2(x, y, h);
-                    printStep(outStream, x, y, localError);
+                    double ynew = rungeK2(oldx, y, x - oldx);
+                    printStep(outStream, x, ynew, localError);
                 }
                 double oldx = x;
                 x = endPoint;
-                y = rungeK2(x, y, x - oldx);
-                printStep(outStream, x, y, localError);
+                double ynew = rungeK2(oldx, y, x - oldx);
+                printStep(outStream, x, ynew, localError);
+                y = ynew;
             }
             else {
+                printStep(outStream, x, y, localError);
+                y = y2;
+                stepsCount++;
                 x += h;
+                if (localError < data.eps / 4) {
+                    h *= 2.0;
+                }
             }
         } else {
             errorStepsCount++;
